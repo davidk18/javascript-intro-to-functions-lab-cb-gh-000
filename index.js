2ccc2076859cc0c10c59e2cf3f9d486f37b2281a
David Kearney <15169235@studentmail.ul.ie>
2017-11-16 11:29:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,8 @@ function sayHiToGrandma(string){
   if(string === x)
   return "YES INDEED!";
 }
+
+function sayHiToGrandma(string){
+  if (string.equals("I love you Grandma."))
+    return "I love you, too.";
+}
